3c499bb68b5b070fdf8e4da9355a8b8fb2df5ca5
DiegoRenner <diego.renner@hotmail.com>
2021-07-16 21:37:40 +0200

    added biography page

--- biography.php
+++ biography.php
@@ -8,11 +8,12 @@
 </head>
 <body>
 <div class="gallery">
-   <img src="
+   <img src="images/portrait.jpg">
 </div>
 <div class="text">
-
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 
+<img onclick="location.href='index.php'" id="home_button" src="images/home.png">
 </body>
 </html>
